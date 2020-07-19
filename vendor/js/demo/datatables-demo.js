bbff2f71b8f8755603d9c398d5d7e1b13074715d
didihansya <didihansya@didihansyas-MacBook-Air.local>
2020-07-19 13:07:02 +0700

    third commit

--- vendor/js/demo/datatables-demo.js
+++ vendor/js/demo/datatables-demo.js
@@ -2,3 +2,18 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+
+// $(document).ready(function () {
+//   $('#dataTable').DataTable({
+
+//     columnDefs: [{
+//       orderable: false,
+//       className: 'select-checkbox',
+//       targets: 0
+//     }],
+//     select: {
+//       style: 'multi',
+//       selector: 'td:first-child'
+//     }
+//   });
+// });
